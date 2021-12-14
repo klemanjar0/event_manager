09335868aea2bf603e09a9abbec6199261f5350f
Klym Kravchenko <klymkravchenko@ecisys.com>
2021-12-14 15:25:44 +0200

    user rest implementation fix

--- src/api/http/controllers/api.ts
+++ src/api/http/controllers/api.ts
@@ -1,8 +1,10 @@
 import Controller from './controller';
 import { ExportController } from '../../../decorators';
 import AuthController from '../controllers/auth';
+import UserController from '../controllers/user';
 
 @ExportController(AuthController)
+@ExportController(UserController)
 class ApiController extends Controller {
     constructor() {
         super('/api');
